cb2e2c107e192e05ab66147da5dace6c9eb2aa4d
JixiuChang <ccjixiuchang.2901888682@gmail.com>
2023-09-13 16:08:18 -0500

    Added MainView to App.xaml.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace MyFirstMobileApp
-{
+﻿using MyFirstMobileApp.ViewViewModels.Main;
+
+namespace MyFirstMobileApp{
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new MainView();
         }
     }
 }
